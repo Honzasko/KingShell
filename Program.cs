610ccbec53f6ab1a9facbb3ac21049e132c1e512
Honzasko <straka.jan5@seznam.cz>
2022-02-18 19:58:06 +0100

    add these commands:cd,cat

--- Program.cs
+++ Program.cs
@@ -14,8 +14,6 @@ else
 }
 
 
-String[] directories = Directory.GetDirectories(directory);
-String[] files = Directory.GetFiles(directory);
 
 IDictionary<String, Func<IList<String>, int>> commands = new Dictionary<String, Func<IList<String>, int>>();
 int exitapp(IList<String> com_args) {
@@ -26,6 +24,8 @@ int exitapp(IList<String> com_args) {
 
 
 int list_dir(IList<String> com_args) {
+    String[] directories = Directory.GetDirectories(directory);
+    String[] files = Directory.GetFiles(directory);
     Console.ForegroundColor = ConsoleColor.White;
     Console.BackgroundColor = ConsoleColor.Green;
     for (int i = 0; i < directories.Length; i++) {
@@ -42,8 +42,135 @@ int list_dir(IList<String> com_args) {
     return 1;
 }
 
+int change_dir(IList<String> com_args) {
+    if (com_args.Count == 1)
+    {
+        String[] directories = Directory.GetDirectories(directory);
+        for (int i = 0; i < directories.Length; i++) {
+          directories[i] = directories[i].Replace(directory, ""); ;
+        }
+        if (directories.Contains('\\' + com_args[0]))
+        {
+            directory += "/" + com_args[0];
+            return 1;
+        }
+        else {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("cd:This directory does not exists");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+    else {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("cd:Invalid usage");
+        Console.ResetColor();
+        return 0;
+    }
+}
+
+int cat_cmd(IList<String> com_args) {
+    if (com_args.Count == 1)
+    {
+        String[] files = Directory.GetFiles(directory);
+        for (int i = 0; i < files.Length; i++)
+        {
+            files[i] = files[i].Replace(directory, ""); ;
+        }
+        if (files.Contains('\\' + com_args[0]))
+        {
+            String content = File.ReadAllText(directory + com_args[0]);
+            Console.WriteLine(content);
+            return 1;
+        }
+        else {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("cat:This file doesnt seems to exist");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+    else if (com_args.Count > 1)
+    {
+        IList<String> input = new List<String>();
+        String output = "";
+        bool change = false;
+        for (int k = 0; k < com_args.Count; k++)
+        {
+            if (!change)
+            {
+                if (com_args[k] != "<")
+                {
+                    input.Add(com_args[k]);
+                }
+                else
+                {
+                    if (k == (com_args.Count - 2))
+                    {
+                        change = true;
+                    }
+
+                }
+            }
+            else
+            {
+                output = com_args[k];
+            }
+        }
+        if (change)
+        {
+            String[] files = Directory.GetFiles(directory);
+            for (int i = 0; i < files.Length; i++)
+            {
+                files[i] = files[i].Replace(directory, ""); ;
+            }
+            int valid = 0;
+            for (int l = 0; l < input.Count; l++)
+            {
+                if (files.Contains('\\' + input[l]))
+                {
+                    valid++;
+                }
+            }
+            if (valid == input.Count)
+            {
+                String content = "";
+                for (int j = 0; j < input.Count; j++)
+                {
+                    String current = File.ReadAllText(directory + input[j]);
+                    content += current;
+                }
+                File.WriteAllText(directory + output, content);
+                return 1;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("cat:Some file/files dont/doesnt exist");
+                Console.ResetColor();
+                return 0;
+            }
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("cat:Invalid usage");
+            Console.ResetColor();
+            return 0;
+        }
+
+    }
+    else {
+        return 0;
+    }
+    
+}
+
+
 commands["exit"] = exitapp;
 commands["ls"] = list_dir;
+commands["cd"] = change_dir;
+commands["cat"] = cat_cmd;
 
 while (!exit) {
     Console.ForegroundColor = ConsoleColor.Yellow;
@@ -55,7 +182,7 @@ while (!exit) {
     IList<String> arguments = new List<String>();
     if (c_args.Length > 1)
     {
-        for (int i = 0; i < c_args.Length; i++)
+        for (int i = 1; i < c_args.Length; i++)
         {
             arguments.Add(c_args[i]);
         }
